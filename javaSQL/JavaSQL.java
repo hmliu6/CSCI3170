039431e4480a7a53d773567c5e52e5fb669139d5
Liu Ho Man <hmliu6@gmail.com>
2017-11-27 14:51:19 +0800

    Add functions template for user

--- javaSQL/JavaSQL.java
+++ javaSQL/JavaSQL.java
@@ -16,8 +16,7 @@ public class JavaSQL {
         try{
             Class.forName("com.mysql.jdbc.Driver");
             conn = DriverManager.getConnection(url, username, password);
-            testfunc(conn);
-            main_menu();
+            main_menu(conn);
             conn.close();
         }
         catch (SQLException ex){
@@ -31,31 +30,36 @@ public class JavaSQL {
         }
     }
 
-    public static void main_menu(){
+    public static void main_menu(Connection conn){
+      int input;
       System.out.println("\n-----Main Menu-----");
       System.out.println("What kinds of operations would you like to perform?");
       System.out.println("1. Operations for administrator");
       System.out.println("2. Operations for library user");
       System.out.println("3. Operations for librarian");
       System.out.println("4. Exit this program");
-      System.out.print("Enter Your Choice: ");
-      Scanner scan = new Scanner(System.in);
-      int input = scan.nextInt();
+      do{
+        System.out.print("Enter Your Choice: ");
+        Scanner scan = new Scanner(System.in);
+        input = scan.nextInt();
+        scan.close();
+      } while(input > 0 && input < 5);
       if(input == 1)
-        admin_operation();
+        admin_operation(conn);
       else if(input == 2)
-        user_operation();
+        user_operation(conn);
       else if(input == 3)
-        librarian_operation();
+        librarian_operation(conn);
       else if(input == 4){
         System.out.println("GoodBye!!!");
         System.exit(1);
       }
       else
-        main_menu();
+        main_menu(conn);
     }
 
-    public static void admin_operation(){
+    public static void admin_operation(Connection conn){
+      int input;
       System.out.println("\n-----Operations for administrator menu-----");
       System.out.println("What kinds of operations would you like to perform?");
       System.out.println("1. Create all tables");
@@ -63,9 +67,12 @@ public class JavaSQL {
       System.out.println("3. Load Data");
       System.out.println("4. Show number of records in each table");
       System.out.println("5. Return to the main menu");
-      System.out.print("Enter Your Choice: ");
-      Scanner scan = new Scanner(System.in);
-      int input = scan.nextInt();
+      do{
+        System.out.print("Enter Your Choice: ");
+        Scanner scan = new Scanner(System.in);
+        input = scan.nextInt();
+        scan.close();
+      } while(input > 0 && input < 6);
       if(input == 1)
         System.exit(1);
       else if(input == 2)
@@ -75,40 +82,46 @@ public class JavaSQL {
       else if(input == 4)
         System.exit(1);
       else if(input == 5)
-        main_menu();
-      else
-        admin_operation();
+        main_menu(conn);
     }
 
-    public static void user_operation(){
+    public static void user_operation(Connection conn){
+      int input;
       System.out.println("\n-----Operations for library user menu-----");
       System.out.println("What kinds of operations would you like to perform?");
       System.out.println("1. Search for books");
       System.out.println("2. Shoe checkout records of a user");
       System.out.println("3. Return to the main menu");
-      System.out.print("Enter Your Choice: ");
-      Scanner scan = new Scanner(System.in);
-      int input = scan.nextInt();
+      do{
+        System.out.print("Enter Your Choice: ");
+        Scanner scan = new Scanner(System.in);
+        input = scan.nextInt();
+        scan.close();
+      } while(input > 0 && input < 4);
       if(input == 1)
-        System.exit(1);
+        bookSearch(conn);
       else if(input == 2)
-        System.exit(1);
+        showUserRecord(conn);
       else if(input == 3)
-        main_menu();
+        main_menu(conn);
       else
-        user_operation();
+        user_operation(conn);
     }
 
-    public static void librarian_operation(){
-      System.out.println("\n-----Operations for administrator menu-----");
+    public static void librarian_operation(Connection conn){
+      int input;
+      System.out.println("\n-----Operations for librarian menu-----");
       System.out.println("What kinds of operations would you like to perform?");
       System.out.println("1. Book Borrowing");
       System.out.println("2. Book Returning");
       System.out.println("3. List all un-returned book copies which are checked out within a period");
       System.out.println("4. Return to the main menu");
-      System.out.print("Enter Your Choice: ");
-      Scanner scan = new Scanner(System.in);
-      int input = scan.nextInt();
+      do{
+        System.out.print("Enter Your Choice: ");
+        Scanner scan = new Scanner(System.in);
+        input = scan.nextInt();
+        scan.close();
+      } while(input > 0 && input < 5);
       if(input == 1)
         System.exit(1);
       else if(input == 2)
@@ -116,17 +129,36 @@ public class JavaSQL {
       else if(input == 3)
         System.exit(1);
       else if(input == 4)
-        main_menu();
+        main_menu(conn);
       else
-        librarian_operation();
+        librarian_operation(conn);
     }
 
-    public static void testfunc(Connection conn){
+    public static void bookSearch(Connection conn){
+      int input;
+      System.out.println("Choose the search criteria:");
+      System.out.println("1. Call Number");
+      System.out.println("2. Title");
+      System.out.println("3. Author");
+      do{
+        System.out.print("Choose the search criteria: ");
+        Scanner scan = new Scanner(System.in);
+        input = scan.nextInt();
+        scan.close();
+      } while(input > 0 && input < 4);
       try{
         Statement stmt = conn.createStatement();
+        stmt.close();
       }
       catch (Exception exp){
         System.out.println("Exception: " + exp.getMessage());
       }
+    }
+
+    public static void showUserRecord(Connection conn){
+      System.out.print("Enter the User ID: ");
+      Scanner scan = new Scanner(System.in);
+      String input = scan.nextLine();
+      scan.close();
     }
 }
