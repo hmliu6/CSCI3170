d9327b526f53c6cc562cbb5d47beb6ccd7aa6be8
Liu Ho Man <hmliu6@gmail.com>
2017-11-28 10:56:19 +0800

    Updated docker commands to store mysql database permanently

--- javaSQL/hostMySQL.sh
+++ javaSQL/hostMySQL.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=test centurylink/mysql
+docker run -d -v ~/Desktop/mysql:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=test centurylink/mysql
